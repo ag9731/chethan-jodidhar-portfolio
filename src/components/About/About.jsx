4cca0b6d5e0937609b8a9947274d1317cea92923
Akash <goravanakollakash@gmail.com>
2025-01-07 18:19:15 +0530

    first commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,6 +3,7 @@ import About_Us_Banner_Section from './components/About_Us_Banner_Section'
 import About_Us_Section2 from './components/About_Us_Section2'
 import AboutUs_Colloborative_Team from './components/AboutUs_Colloborative_Team'
 import About_Us_Cinemetography from './components/About_Us_Cinemetography'
+import About_Us_Skills from './components/About_Us_Skills'
 
 const About = () => {
   return (
@@ -11,6 +12,7 @@ const About = () => {
       <About_Us_Section2/>
       <About_Us_Cinemetography/>
       <AboutUs_Colloborative_Team/>
+      <About_Us_Skills/>
     </div>
   )
 }
